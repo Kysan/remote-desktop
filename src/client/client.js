f59d9fee56c33624c2d16c16551c054b3974c9fc
Kysan <42589767+Kysan@users.noreply.github.com>
2022-09-25 23:00:52 +0200

    working

--- src/client/client.js
+++ src/client/client.js
@@ -1,15 +1,57 @@
 
+
+
+const screen = document.getElementById("screen")
 const url = document.location.host;
 const client = new WebSocket(`ws://${url}`)
 
 
-client.onopen = () => {
-    document.getElementById("info").textContent = "connecté !"
-
-    window.addEventListener("keypress", ({ key }) => {
-        fetch("input/keypress", {
-            method: "POST",
-            body: JSON.stringify({ key })
-        })
+const click = (x, y, button) => {
+    fetch("input/click", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ x: x, y: y, button })
     })
+}
+
+
+const pressKeyBoard = (key) => {
+    fetch("input/keypress", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ key })
+    })
+}
+
+
+client.onopen = () => {
+    console.log("connected")
+
+    // * keyboard
+    window.addEventListener("keypress", ({ key }) => pressKeyBoard(key))
+
+    // * right click
+    screen.addEventListener('contextmenu', ev => {
+        ev.preventDefault();
+        const { x, y } = ev;
+
+        click(x, y, "right")
+    });
+
+
+    // * left click
+    screen.addEventListener('click', (ev) => {
+        const { x, y, which } = ev;
+
+        click(x, y, "left")
+    })
+}
+
+
+client.onmessage = ({ data }) => {
+    screen.src = `data:image/png;base64, ${data}`
 }
--- src/server/server.js
+++ src/server/server.js
@@ -3,13 +3,10 @@ const robot = require("robotjs")
 const http = require("http");
 const ws = require("ws")
 const path = require("path")
-
+const jimp = require("jimp")
 
 const app = express()
 app.use(express.json())
-
-
-
 
 
 const server = http.createServer(app);
@@ -21,10 +18,12 @@ app.use('/', express.static(staticPath));
 wsServer.on("connection", (socket) => {
     console.log("new connection")
 
-    setInterval(() => {
+    setInterval(async () => {
         const screenshot = robot.screen.capture()
-
-    }, 500)
+        const img = convert(screenshot)
+        const buff = await img.getBufferAsync(jimp.MIME_PNG)
+        socket.send(buff.toString('base64'))
+    }, 50)
 })
 
 
@@ -32,15 +31,19 @@ app.get("/ping", (_, res) => res.send("pong"))
 
 // * mouse control
 app.post("/input/click", (req) => {
-    const { x, y, click } = req.body;
-
-    robot.moveMouse(x, y)
-    robot.mouseClick(click)
+    const { x, y, button } = req.body;
+    // Accepts left, right, or middle.
+    console.log({ x, y })
+    robot.moveMouseSmooth(x, y)
+    robot.mouseClick("left")
 })
 
 // * keyboard control
 app.post("/input/keypress", (req) => {
     const { key } = req.body;
+
+    if (!"abcdefghijklmnopqrstuvwxyz0123456789".includes(key)) return;
+
     console.log({ key })
     robot.keyTap(key)
 
@@ -59,10 +62,25 @@ app.post("/input/scroll", (req) => {
 
 
 
+const convert = (image) => {
+    for (let i = 0; i < image.image.length; i++) {
+        if (i % 4 == 0) {
+            [image.image[i], image.image[i + 2]] = [image.image[i + 2], image.image[i]];
+        }
+    }
+
+    let jimg = new jimp(image.width, image.height);
+    jimg.bitmap.data = image.image;
+    return jimg
+}
+
 
 
 
 
 const { port } = require("../../config.json")
 
-server.listen(port, () => console.log(`listening on port ${port}`))
+server.listen(port, () => {
+    const { interfaces } = require("./NetworkUtils")
+    console.log({ listeningPort: port, interfaces })
+})
